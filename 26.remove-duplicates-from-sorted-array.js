948edce4fbd04e52732e71056f7ce1d1b1e857f4
rqcao <rqcao@outlook.com>
2020-04-12 22:07:49 +0800

    通过解答。26. 删除排序数组中的重复项

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -43,14 +43,16 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  return  nums.reduce((total, curValue, curIndex, arr) => {
-    if(total.indexOf(curValue) === -1){
-      total.push(curValue)
+  let i = 0;
+  while (i++ < nums.length) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i--;
     }
-    return total
-  },[]).length;
+  }
+  return nums.length;
 };
 
-var a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+var a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6];
 var b = removeDuplicates(a);
 console.log(b);
