9c03839dd8df68745b7cf1525754790c7512dfe7
rqcao <rqcao@outlook.com>
2020-04-12 23:05:48 +0800

    增加双指针法

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -53,6 +53,21 @@ var removeDuplicates = function (nums) {
   return nums.length;
 };
 
+// var removeDuplicates = function (nums) {
+//   if (nums.length === 0) {
+//       return 0;
+//   }
+//   let j = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//       if (nums[i] !== nums[j]) {
+//           j++;
+//           nums[j] = nums[i];
+//       }
+//   }
+//   // return j + 1;
+//   return nums;
+// };
+
 var a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6];
 var b = removeDuplicates(a);
 console.log(b);
