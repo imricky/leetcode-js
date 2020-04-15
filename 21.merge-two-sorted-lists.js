91fee82aefbc8d78197f1cd604a1543d86091a25
rqcao <rqcao@outlook.com>
2020-04-15 09:56:09 +0800

    21.合并两个有序链表  新增递归解法

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -23,21 +23,41 @@
  * @return {ListNode}
  */
 
-var mergeTwoLists = function(l1, l2) {
+// 迭代解法
+var mergeTwoLists = function (l1, l2) {
   var res = new ListNode(null);
   var pre = res;
-  while(l1 && l2){
-    if(l1.val < l2.val){
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
       pre.next = new ListNode(l1.val);
       l1 = l1.next ? l1.next : null;
-    }else{
+    } else {
       pre.next = new ListNode(l2.val);
       l2 = l2.next ? l2.next : null;
     }
     pre = pre.next;
   }
-  if(l1 || l2){
+  if (l1 || l2) {
     pre.next = l1 ? l1 : l2;
   }
   return res.next;
 };
+
+// 递归解法
+var mergeTwoLists = function (l1, l2) {
+  if (l1 === null) {
+    return l2
+  }
+  if (l2 === null) {
+    return l1
+  }
+
+  if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
+
+}
